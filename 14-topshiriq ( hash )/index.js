10b1c170908bdc3c0f4c487f0284ad7fed2987e9
devona-dev <mamka.islomv@gmail.com>
2022-08-10 10:32:52 +0500

    added user file >>hash_coding<<

---
+++ 14-topshiriq ( hash )/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const categoriesRoute = require('./routes/categories');
+const customersRoute = require('./routes/customers');
+const coursersRoute = require('./routes/coursers');
+const enrollmentRoute = require('./routes/enrollments');
+const usersRoute = require('./routes/users');
+const app = express();
+app.use(express.json());
+app.use('/api/categories', categoriesRoute);
+app.use('/api/customers', customersRoute)
+app.use('/api/coursers', coursersRoute)
+app.use('/api/enrollments', enrollmentRoute);
+app.use('/api/users', usersRoute);
+
+mongoose.connect('mongodb://localhost/virtualDars')
+    .then(()=>{
+        console.log('MongoDB ga ulandi');
+    })
+    .catch((err)=>{
+        console.error('ulanishda xatolik', err);
+    })
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`${port}chi portni eshitishni boshladim...`);
+});
---
+++ 14-topshiriq ( hash )/routes/enrollments.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const Joi = require('joi');
+const router = express.Router();
+const { Courser } = require('../models/courser');
+const { Customer } = require('../models/customer');
+const { Enrollment } = require('../models/enrollment');
+
+
+
+router.get('/', async (req, res) => {
+    const enrollments = await Enrollment.find().sort('-dateStart');
+    res.send(enrollments);
+});
+
+router.post('/', async (req, res) => {
+    const schema = Joi.object({
+        customerId: Joi.string().required(),
+        courseId: Joi.string().required()
+    })
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const course = await Courser.findById(req.body.courseId);
+    if(!course)
+        res.status(404).send('Bunaqa ID mavjud emas..')
+
+    const customer = await Customer.findById(req.body.customerId);
+    if(!customer)
+        res.status(404).send('Bunaqa ID mavjud emas..');
+
+    let enrollment = new Enrollment ({
+        customer: {
+            _id: customer._id,
+            name: customer.name
+        },
+        course: {
+            _id: course._id,
+            title: course.title
+        },
+        courseFee: course.fee,
+    });
+    if(customer.isVip)
+        enrollment.courseFee = course.fee - (0.2 * course.fee);
+
+    enrollment = await enrollment.save();
+
+    customer.bonusPoints++;
+    customer.save();
+    
+    res.status(201).send(enrollment);
+});
+
+router.get('/:id', async (req, res) => {
+
+    const enrollment = await Enrollment.findById(req.params.id);
+    if (!enrollment)
+        return res.status(404).send('Berilgan IDga teng bo\'lgan toifa topilmadi');
+
+    res.send(enrollment);
+});
+
+
+module.exports = router;
