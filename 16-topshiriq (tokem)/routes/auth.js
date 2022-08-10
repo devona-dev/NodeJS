a84407951274d5f71454b5dcdbbee35ee2c80e8c
devona-dev <mamka.islomv@gmail.com>
2022-08-10 12:02:24 +0500

    added token in users file...

---
+++ 16-topshiriq (tokem)/routes/auth.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
+const router = express.Router();
+const {  User } = require('../models/user');
+const _= require('lodash');
+const jwt = require('jsonwebtoken');
+
+router.post('/', async (req, res) => {
+    const schema = Joi.object({
+        email: Joi.string().min(5).max(50).required(),
+        password: Joi.string().min(5).max(50).required()
+   })
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    let user = await User.findOne({ email: req.body.email });
+        if(!user)
+            return res.status(400).send('Email yoki parol notogri...');
+            
+    const isvalidPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!isvalidPassword)
+        return res.status(400).send('Email yoki parol notogri...');
+
+    const token = jwt.sign({_id: user._id}, '8bejrewjfsfJKLFKSDJ');
+    res.header('x-auth-token', token).send(true)
+       
+});
+
+module.exports = router;
